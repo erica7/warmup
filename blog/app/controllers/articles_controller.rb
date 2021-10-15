4e52cc4805a388b2971e1e208e148e3c6f8653db
captain <captain@galaxie.local>
2021-10-15 11:06:20 -0400

    add Articles controller; add route; update view

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+class ArticlesController < ApplicationController
+  def index
+    # From Ruby On Rails documentation:
+    #   When an action does not explicitly render a view 
+    #   (or otherwise trigger an HTTP response), Rails will 
+    #   automatically render a view that matches the name 
+    #   of the controller and action. 
+    #   Convention Over Configuration!
+  end
+end
